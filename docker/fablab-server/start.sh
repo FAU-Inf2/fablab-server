d19bcfdd9f59cbce8d4d86c1f0c3c23e846bfe85
Sebastian Haubner <sebastian.haubner@fau.de>
2015-05-04 17:25:07 +0200

    fix docker startup script to pass environment vars to docker container

--- docker/fablab-server/start.sh
+++ docker/fablab-server/start.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
-docker run -p 80:8080 -p 8081:8081 container-fablab-server
+docker run -p 80:8080 -p 8081:8081 -e adminUsername=$adminUsername -e adminPassword=$adminPassword container-fablab-server
+

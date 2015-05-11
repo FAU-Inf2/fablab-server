ba7153c1c5cfbd03b394ffd6b27ea5a7b2a316d2
Max Jalowski <max.jalowski@fau.de>
2015-05-11 17:52:33 +0200

    Added env-vars for usernames and passwords and restart script

--- docker/drupal/runme.sh
+++ docker/drupal/runme.sh
@@ -6,9 +6,9 @@ docker build -t basic-drupal .
 docker build -t mysql-for-drupal mysql/
 
 # run mysql-docker
-docker run -e MYSQL_ROOT_PASSWORD=test123 -e MYSQL_DATABASE=drupal -e MYSQL_USER=drupal -e MYSQL_PASSWORD=test123 --name mysql-for-drupal -p 127.0.0.1:53306:3306 -d mysql:latest
+docker run -e MYSQL_ROOT_PASSWORD=$adminPassword -e MYSQL_DATABASE=drupal -e MYSQL_USER=$drupalDBUser -e MYSQL_PASSWORD=$drupalPassword --name mysql-for-drupal -p 127.0.0.1:53306:3306 -d mysql:latest
 
 # run drupal-docker
-docker run -d -p 80:80 --link mysql-for-drupal:db -i -t --name basic-drupal-v1 basic-drupal:latest
+docker run -e adminDrupal=$drupalAdmin -e adminPassword=$drupalPassword -d  -p 80:80 --link mysql-for-drupal:db -i -t --name basic-drupal-v1 basic-drupal:latest
 
 docker ps
--- docker/drupal/start.sh
+++ docker/drupal/start.sh
@@ -6,16 +6,35 @@ then
 	echo "/var/www/sites/default/settings.php does not exist -> start site-install"
 	sleep 5s
 	cd /var/www/
-	drush site-install standard -y --account-name=admin --account-pass=admin --db-url="mysqli://${DB_ENV_MYSQL_USER}:${DB_ENV_MYSQL_PASSWORD}@${DB_PORT_3306_TCP_ADDR}:${DB_PORT_3306_TCP_PORT}/drupal"
+	drush site-install standard -y --account-name=${adminDrupal} --account-pass=${adminPassword} --db-url="mysqli://${DB_ENV_MYSQL_USER}:${DB_ENV_MYSQL_PASSWORD}@${DB_PORT_3306_TCP_ADDR}:${DB_PORT_3306_TCP_PORT}/drupal"
+
+	# save host
+	#export DB_HOST=${DB_PORT_3306_TCP_ADDR}
+	#echo "DB_HOST=${DB_HOST}" >> /etc/environment 
+	echo ${DB_PORT_3306_TCP_ADDR} > /oldhost.txt
 
 	chown -R www-data:www-data /var/www/sites/default
 
 	# TODO install modules
+	echo "test installing module:"
+	drush en fullcalendar -y
+	#drush en notifications -y
 
 	a2enmod rewrite vhost_alias
 	/etc/init.d/apache2 restart
 	sleep 5s
 else
 	echo "/var/www/sites/default/settings.php already exists.. skipped site-install"
+	cd /var/www/sites/default/
+
+	old_host=$(cat /oldhost.txt)
+	echo ${old_host}
+
+	sed -i s/${old_host}/${DB_PORT_3306_TCP_ADDR}/g settings.php
+#	sed /host/s/\b${DB_OLD_HOST}\b/${DB_PORT_3306_TCP_ADDR}/g settings.php
+
+	# save host
+	echo ${DB_PORT_3306_TCP_ADDR} > /oldhost.txt
+
 	/etc/init.d/apache2 restart
 fi

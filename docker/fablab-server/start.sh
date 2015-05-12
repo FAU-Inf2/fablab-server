59a6b1367a20b5e94ef789f7b79657ad00939558
Sebastian Haubner <sebastian.haubner@fau.de>
2015-05-12 23:25:06 +0200

    more environment vars for fablab-server docker container

--- docker/fablab-server/start.sh
+++ docker/fablab-server/start.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-docker run -d -p 80:8080 -p 8081:8081 --name="rest-server" -e adminUsername=$adminUsername -e adminPassword=$adminPassword container-fablab-server
+docker run -d -p 80:8080 -p 8081:8081 --name="rest-server" -e openerp_hostname=$openerp_hostname -e openerp_database=$openerp_database -e openerp_user=$openerp_user -e openerp_password=$openerp_password -e adminUsername=$adminUsername -e adminPassword=$adminPassword container-fablab-server

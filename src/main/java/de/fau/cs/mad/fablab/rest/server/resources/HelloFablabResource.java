7c8e7be7a7aa980e24a1965982ff05151b1bd62d
Sebastian Haubner <sebastian.haubner@fau.de>
2015-05-12 22:41:29 +0200

    add example for the openerp interface usage

--- src/main/java/de/fau/cs/mad/fablab/rest/server/resources/HelloFablabResource.java
+++ src/main/java/de/fau/cs/mad/fablab/rest/server/resources/HelloFablabResource.java
@@ -2,31 +2,44 @@ package de.fau.cs.mad.fablab.rest.server.resources;
 
 import com.codahale.metrics.annotation.Timed;
 import com.google.common.base.Optional;
+import de.fau.cs.mad.fablab.rest.entities.Product;
 import de.fau.cs.mad.fablab.rest.entities.WelcomeUser;
+import de.fau.cs.mad.fablab.rest.server.openerp.OpenErpClient;
+import de.fau.cs.mad.fablab.rest.server.openerp.OpenErpInterface;
 import io.dropwizard.jersey.params.BooleanParam;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
+import java.net.MalformedURLException;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 
-/***
+/**
  * Resource class for our /hello-fablab uri
  * NOTE: This class is being used by multiple threads concurrently
  */
 @Path("/hello-fablab")
 @Produces(MediaType.APPLICATION_JSON)
-public class HelloFablabResource
-{
+public class HelloFablabResource {
     private final String template;
     private final String defaultName;
     private final AtomicLong counter;
     private final AtomicLong greetCounter;
+
+    OpenErpInterface mOpenErp;
 
     public HelloFablabResource(String template, String defaultName) {
         this.template = template;
         this.defaultName = defaultName;
         this.counter = new AtomicLong();
         greetCounter = new AtomicLong();
+
+        mOpenErp = new OpenErpClient();
+        try {
+            mOpenErp.authenticate();
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
     }
 
     @GET
@@ -39,9 +52,37 @@ public class HelloFablabResource
     @POST
     @Timed
     public WelcomeUser greetOtherUsers(@QueryParam("greet") BooleanParam greet,
-                                       @QueryParam("name") Optional<String> name){
+                                       @QueryParam("name") Optional<String> name) {
         final String nameVal = String.format(template, name.or(defaultName));
-        if(greet.get()) greetCounter.incrementAndGet();
+        if (greet.get()) {
+            greetCounter.incrementAndGet();
+        }
         return new WelcomeUser(counter.incrementAndGet(), nameVal, greetCounter.get());
+    }
+
+    /**
+     * Search and return products which contain the search string in their name
+     * @param name the search string
+     * @param max  the max number of elements returned
+     * @return a list of {@link Product} entries
+     */
+    @GET
+    @Path("/products/search")
+    public List<Product> searchProductByName(@QueryParam("name") String name,
+                                             @QueryParam("max") int max) {
+        return mOpenErp.searchForProducts(name, max, 0);
+    }
+
+    /***
+     * Receives a list of {@link Product} from a given openerp instance
+     * @param max the max number of elements returned
+     * @param offset the offset where to start
+     * @return a list of {@link Product} entries
+     */
+    @GET
+    @Path("/products")
+    public List<Product> getProducts(@QueryParam("max") int max,
+                                     @QueryParam("offset") int offset) {
+        return mOpenErp.getProducts(max, offset);
     }
 }

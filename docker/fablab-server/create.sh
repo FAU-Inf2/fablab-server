93c2274f315d8757d468982f0e2f81b04d71d01b
Sebastian Haubner <sebastian.haubner@fau.de>
2015-05-17 23:32:57 +0200

    update Dockerfile and shell scripts for keystore usage

--- docker/fablab-server/create.sh
+++ docker/fablab-server/create.sh
@@ -1,3 +1,16 @@
 #!/bin/sh
 
+
+if [ ! -f fablab-rest-server.keystore ]; then
+	echo "[INFO] - Missing keystore file! looking for it in ../../src/dist"
+	if [ ! -f ../../src/dist/fablab-rest-server.keystore ]; then
+		echo "[ERROR] - No keystore file found in src/dist. please copy it over from docs repo"
+		echo "Quitting...."
+		exit
+	fi
+	echo "[INFO] - Copying keystore file from ../../src/dist to current dir."
+	cp ../../src/dist/fablab-rest-server.keystore .
+fi
+
+echo "Creating container..."
 docker build -t container-fablab-server .
--- docker/fablab-server/start.sh
+++ docker/fablab-server/start.sh
@@ -1,3 +1,12 @@
 #!/bin/sh
 
-docker run -d -p 80:8080 -p 8081:8081 --name="rest-server" -e openerp_hostname=$openerp_hostname -e openerp_database=$openerp_database -e openerp_user=$openerp_user -e openerp_password=$openerp_password -e adminUsername=$adminUsername -e adminPassword=$adminPassword container-fablab-server
+docker run -d \
+    -p 80:8080 -p 8081:8081 -p 443:8082 --name="rest-server" \
+    -e openerp_hostname=$openerp_hostname \
+    -e openerp_database=$openerp_database \
+    -e openerp_user=$openerp_user \
+    -e openerp_password=$openerp_password \
+    -e adminUsername=$adminUsername \
+    -e adminPassword=$adminPassword \
+    -e FABLAB_KEYSTORE_PASSWORD=$FABLAB_KEYSTORE_PASSWORD \
+    container-fablab-server

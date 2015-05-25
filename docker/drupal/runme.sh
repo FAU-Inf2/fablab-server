464e98189f6c5a595ced4c73054072b8a7c42a74
Max Jalowski <max.jalowski@fau.de>
2015-05-25 17:04:02 +0200

    Merged changes from drupal-wip to docker/drupal. Drupal has now a working REST-service and uses a database-dump from the original Drupal-Site

--- docker/drupal/runme.sh
+++ docker/drupal/runme.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # build drupal-docker
-docker build -t basic-drupal .
+docker build -t fablab-drupal .
 # build mysql-docker
 docker build -t mysql-for-drupal mysql/
 
@@ -9,6 +9,6 @@ docker build -t mysql-for-drupal mysql/
 docker run -e MYSQL_ROOT_PASSWORD=$adminPassword -e MYSQL_DATABASE=drupal -e MYSQL_USER=$drupalDBUser -e MYSQL_PASSWORD=$drupalPassword --name mysql-for-drupal -p 127.0.0.1:53306:3306 -d mysql:latest
 
 # run drupal-docker
-docker run -e adminDrupal=$drupalAdmin -e adminPassword=$drupalPassword -d  -p 80:80 --link mysql-for-drupal:db -i -t --name basic-drupal-v1 basic-drupal:latest
+docker run -e adminDrupal=$drupalAdmin -e adminPassword=$drupalPassword -d  -p 50080:80 --link mysql-for-drupal:db -i -t --name fablab-drupal-v1 fablab-drupal:latest
 
 docker ps

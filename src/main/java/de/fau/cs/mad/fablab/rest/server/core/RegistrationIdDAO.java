b40ff6d5f24a20d592bbd53241516f3045e3fd18
johannes_rechner_home <jopf88@googlemail.com>
2015-05-31 21:48:26 +0200

    - Added RegistrationDAO to use db operations

---
+++ src/main/java/de/fau/cs/mad/fablab/rest/server/core/RegistrationIdDAO.java
@@ -0,0 +1,46 @@
+package de.fau.cs.mad.fablab.rest.server.core;
+
+import de.fau.cs.mad.fablab.rest.core.RegistrationId;
+import io.dropwizard.hibernate.AbstractDAO;
+import org.hibernate.Query;
+import org.hibernate.SessionFactory;
+
+import java.util.List;
+
+
+public class RegistrationIdDAO extends AbstractDAO<RegistrationId> {
+
+    public RegistrationIdDAO(SessionFactory sessionFactory) {
+        super(sessionFactory);
+    }
+
+    public RegistrationId create(RegistrationId aRegistrationId){
+        return persist(aRegistrationId);
+    }
+
+    public RegistrationId update(RegistrationId aModifiedRegistrationId){
+        RegistrationId storedRegistrationId = get(aModifiedRegistrationId.getRegistrationid_id());
+        storedRegistrationId.setRegistrationid(aModifiedRegistrationId.getRegistrationid());
+        persist(storedRegistrationId);
+        return storedRegistrationId;
+    }
+
+    public List<RegistrationId> findAll(){
+        Query query = currentSession().createQuery("FROM RegistrationId");
+        return query.list();
+    }
+
+    public boolean delete(long registrationId_id) {
+        System.out.println("DELETED: " + String.valueOf(registrationId_id));
+        if (get(registrationId_id) == null)
+            return false;
+
+        currentSession().delete(get(registrationId_id));
+        return true;
+    }
+
+    public void deleteAll(){
+        System.out.println("DELETED: ALL!!! ");
+        currentSession().createQuery("delete FROM RegistrationId").executeUpdate();
+    }
+}

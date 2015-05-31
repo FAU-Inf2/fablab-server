69a6011e39373d3016fa7c61a2e4b40a23de08d1
Philip Kanhaeusser <git@mail.philip.xers.de>
2015-06-01 00:24:33 +0200

    Made DoorState serializable

--- src/main/java/de/fau/cs/mad/fablab/rest/server/core/doorstate/DoorState.java
+++ src/main/java/de/fau/cs/mad/fablab/rest/server/core/doorstate/DoorState.java
@@ -4,6 +4,7 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import java.io.Serializable;
 
 /**
  * POJO to represent the door state
@@ -13,7 +14,7 @@ import javax.persistence.Table;
  */
 @Entity
 @Table(name="DoorState")
-public class DoorState
+public class DoorState implements Serializable
 {
     public final static String DATA_REGEX = "^\\d+:(open|close)$";
     public final static String DELIMITER = ":";

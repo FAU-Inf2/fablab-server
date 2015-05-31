efef64b6a67ecafbcbb87eab6fca5929a80dd453
johannes_rechner_home <jopf88@googlemail.com>
2015-05-31 21:47:42 +0200

    - Added RegistrationIdFacade

---
+++ src/main/java/de/fau/cs/mad/fablab/rest/server/core/RegistrationIdFacade.java
@@ -0,0 +1,34 @@
+package de.fau.cs.mad.fablab.rest.server.core;
+
+import de.fau.cs.mad.fablab.rest.core.RegistrationId;
+import org.hibernate.Query;
+
+import java.util.List;
+
+public class RegistrationIdFacade {
+
+    private RegistrationIdDAO mRegistrationIdDAO;
+
+    public RegistrationIdFacade(RegistrationIdDAO aRegistrationIdDAO){
+        mRegistrationIdDAO = aRegistrationIdDAO;
+    }
+
+    public RegistrationId create(RegistrationId aRegistrationId){
+        return mRegistrationIdDAO.create(aRegistrationId);
+    }
+
+    public RegistrationId update(RegistrationId aModifiedRegistrationId){
+        return mRegistrationIdDAO.update(aModifiedRegistrationId);
+    }
+
+    public List<RegistrationId> findAll(){
+        return mRegistrationIdDAO.findAll();    }
+
+    public boolean delete(long registrationId_id) {
+        return mRegistrationIdDAO.delete(registrationId_id);
+    }
+
+    public void deleteAll(){
+        mRegistrationIdDAO.deleteAll();
+    }
+}

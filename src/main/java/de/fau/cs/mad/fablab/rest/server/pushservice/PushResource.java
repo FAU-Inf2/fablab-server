9ec417726bc768d0a09398847f4be05b0d654165
johannes_rechner_home <jopf88@googlemail.com>
2015-05-31 21:46:27 +0200

    - Added PushResource

---
+++ src/main/java/de/fau/cs/mad/fablab/rest/server/pushservice/PushResource.java
@@ -0,0 +1,40 @@
+package de.fau.cs.mad.fablab.rest.server.pushservice;
+
+import ch.qos.logback.core.net.SyslogOutputStream;
+import de.fau.cs.mad.fablab.rest.api.PushApi;
+import de.fau.cs.mad.fablab.rest.core.RegistrationId;
+import de.fau.cs.mad.fablab.rest.entities.WelcomeUser;
+import de.fau.cs.mad.fablab.rest.server.core.RegistrationIdDAO;
+import de.fau.cs.mad.fablab.rest.server.core.RegistrationIdFacade;
+import io.dropwizard.hibernate.UnitOfWork;
+import io.dropwizard.jersey.params.LongParam;
+
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriBuilder;
+import java.util.List;
+
+
+public class PushResource implements PushApi{
+
+    private RegistrationIdFacade mRegistrationIdFacade;
+
+    public PushResource(RegistrationIdFacade aRegistrationIdFacade){
+        mRegistrationIdFacade = aRegistrationIdFacade;
+    }
+
+    @UnitOfWork
+    @Override
+    public WelcomeUser addRegistrationId(String regId) {
+        System.out.println("RegistrationID: " + regId);
+
+        mRegistrationIdFacade.create(new RegistrationId("112341234"));
+        List<RegistrationId> registrationIdList = mRegistrationIdFacade.findAll();
+
+        for(RegistrationId registrationId : registrationIdList){
+            System.out.println(registrationId.getRegistrationid_id());
+        }
+
+        long l = 1;
+        return new WelcomeUser(l,"asdf",l);
+    }
+}

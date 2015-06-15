b18088c16a2c077ebe3f60c49f0c8622f47bc5eb
johannes_rechner_home <jopf88@googlemail.com>
2015-06-15 21:41:28 +0200

    - Refactord and moved packages

--- src/main/java/de/fau/cs/mad/fablab/rest/server/resources/PushResource.java
+++ src/main/java/de/fau/cs/mad/fablab/rest/server/resources/PushResource.java
@@ -1,10 +1,11 @@
-package de.fau.cs.mad.fablab.rest.server.pushservice;
+package de.fau.cs.mad.fablab.rest.server.resources;
 
 import de.fau.cs.mad.fablab.rest.api.PushApi;
 import de.fau.cs.mad.fablab.rest.core.RegistrationId;
 import de.fau.cs.mad.fablab.rest.server.configuration.PushServiceConfiguration;
 import de.fau.cs.mad.fablab.rest.server.core.RegistrationIdDAO;
 import de.fau.cs.mad.fablab.rest.server.core.RegistrationIdFacade;
+import de.fau.cs.mad.fablab.rest.server.core.pushservice.PushFacade;
 import io.dropwizard.hibernate.UnitOfWork;
 import org.hibernate.SessionFactory;
 

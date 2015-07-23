492ada6d7564818c9b3f4cd9731f03ff2417bf11
Stefan Herpich <Stefan.Herpich@t-online.de>
2015-07-23 20:19:19 +0200

    enabled cors to allow cross domain requests

--- src/main/java/de/fau/cs/mad/fablab/rest/server/ServerApplication.java
+++ src/main/java/de/fau/cs/mad/fablab/rest/server/ServerApplication.java
@@ -26,6 +26,11 @@ import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
 import org.glassfish.jersey.servlet.ServletContainer;
 import org.hibernate.cfg.Configuration;
+import org.eclipse.jetty.servlets.CrossOriginFilter;
+import javax.servlet.DispatcherType;
+import javax.servlet.FilterRegistration;
+
+import java.util.EnumSet;
 
 import java.text.SimpleDateFormat;
 import java.util.Properties;
@@ -124,6 +129,16 @@ class ServerApplication extends Application<ServerConfiguration> {
         dropwizardResourceConfig.register(LogResource.class);
         environment.admin().addServlet("log admin resource", jerseyContainerHolder.getContainer()).addMapping("/admin/*");
 
+        // Enable CORS headers
+   	final FilterRegistration.Dynamic cors = environment.servlets().addFilter("CORS", CrossOriginFilter.class);
+
+    	// Configure CORS parameters
+	cors.setInitParameter("allowedOrigins", "*");
+	cors.setInitParameter("allowedHeaders", "X-Requested-With,Content-Type,Accept,Origin");
+	cors.setInitParameter("allowedMethods", "OPTIONS,GET,PUT,POST,DELETE,HEAD");
+
+	// Add URL mapping
+	cors.addMappingForUrlPatterns(EnumSet.allOf(DispatcherType.class), true, "/*");
     }
 
     public static void main(String[] args) {

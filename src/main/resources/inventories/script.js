40587b363b220bdf8475e8a96fb6b559a15decd2
Emanuel Eimer <mail@eeimer.de>
2015-08-21 06:17:56 +0200

    implemented inventory list  (server/inventories/inventory.html)

--- src/main/resources/inventories/script.js
+++ src/main/resources/inventories/script.js
@@ -1,5 +1,25 @@
 
 
+
+$(document).ready(function() {
+
+    $.get("../../inventory/", function (response) {
+        response.forEach(function(item) {
+            console.log(item);
+            $('#inventoryTable').find('tbody').append('<tr>' +
+                    '<td>' + item.productName +'</td>' +
+                    '<td>' + item.productId +'</td>' +
+                    '<td>' + item.amount +'</td>' +
+                    '<td>' + item.userName +'</td>' +
+                    '<td>' + item.updated_at +'</td>' +
+                '</tr>');
+        });
+    });
+});
+
+
+
+
 function deleteAllTouched(){
     if (confirm("Sicher das die gesamte Liste gelöscht werden soll?")) {
         console.log("del all")

b124649268a3af6e0540b70c2ccd7f4e6c13ba35
Emanuel Eimer <mail@eeimer.de>
2015-08-23 17:16:27 +0200

    improved style CSS for Firefox and IE, added View (div) with "no products in inventory list"

--- src/main/resources/inventories/script.js
+++ src/main/resources/inventories/script.js
@@ -1,17 +1,21 @@
 $(document).ready(function() {
     $.get("../../inventory/", function (response) {
-        response.forEach(function(item) {
-            $("#inventoryTable").show()
-            $("#loading").hide()
-            console.log(item);
-            $('#inventoryTable').find('tbody').append('<tr>' +
-                    '<td>' + item.productName +'</td>' +
-                    '<td>' + item.productId +'</td>' +
-                    '<td>' + item.amount +'</td>' +
-                    '<td>' + item.userName +'</td>' +
-                    '<td>' + item.updated_at +'</td>' +
-                '</tr>');
-        });
+        $("#loading").hide()
+        if(response.length == 0) {
+            $("#empty").show()
+        }else {
+            response.forEach(function (item) {
+                $("#inventoryTable").show()
+                console.log(item);
+                $('#inventoryTable').find('tbody').append('<tr>' +
+                    '<td>' + item.productName + '</td>' +
+                    '<td>' + item.productId + '</td>' +
+                    '<td>' + item.amount + '</td>' +
+                    '<td>' + item.userName + '</td>' +
+                    '<td>' + item.updated_at + '</td>' +
+                    '</tr>');
+            });
+        }
     });
 });
 

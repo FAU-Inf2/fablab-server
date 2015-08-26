b7871e3cfb48ce93f51e25faf49be1f849f3d680
Max Jalowski <max.jalowski@fau.de>
2015-08-26 20:18:57 +0200

    Changed GeneralDataResource to match new DataApi

--- src/main/java/de/fau/cs/mad/fablab/rest/server/resources/GeneralDataResource.java
+++ src/main/java/de/fau/cs/mad/fablab/rest/server/resources/GeneralDataResource.java
@@ -1,6 +1,7 @@
 package de.fau.cs.mad.fablab.rest.server.resources;
 
 import de.fau.cs.mad.fablab.rest.api.DataApi;
+import de.fau.cs.mad.fablab.rest.core.MailAddresses;
 import de.fau.cs.mad.fablab.rest.server.configuration.GeneralDataConfiguration;
 import de.fau.cs.mad.fablab.rest.server.exceptions.Http404Exception;
 import de.fau.cs.mad.fablab.rest.server.exceptions.Http500Exception;
@@ -31,5 +32,14 @@ public class GeneralDataResource implements DataApi {
         }
         if (mail.length() == 0) throw new Http404Exception("Result is empty");
         return mail;
+    }
+
+    @Override
+    public MailAddresses getMailAddresses() {
+        MailAddresses ret = new MailAddresses(config.getFeedbackMail(), config.getFabMail());
+        if (ret == null || ret.getFeedbackMail() == null || ret.getFabLabMail() == null) {
+            throw new Http500Exception("An error occurred while retrieving the Mail-Adresses");
+        }
+        return ret;
     }
 }

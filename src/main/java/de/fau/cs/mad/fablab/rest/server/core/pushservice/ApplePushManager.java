55cb8228b9ed17170374b46c223beba8252d56d6
Emanuel Eimer <mail@eeimer.de>
2015-08-27 20:26:59 +0200

    finished ApplePushManager

--- src/main/java/de/fau/cs/mad/fablab/rest/server/core/pushservice/ApplePushManager.java
+++ src/main/java/de/fau/cs/mad/fablab/rest/server/core/pushservice/ApplePushManager.java
@@ -1,4 +1,5 @@
 package de.fau.cs.mad.fablab.rest.server.core.pushservice;
+import com.relayrides.pushy.apns.util.MalformedTokenStringException;
 import de.fau.cs.mad.fablab.rest.core.CartStatus;
 import de.fau.cs.mad.fablab.rest.server.configuration.ApplePushConfiguration;
 
@@ -21,27 +22,31 @@ public class ApplePushManager implements PushManger{
 
 
     public void sendNotificationDoorJustOpened(List<String> tokens){
-        //TODO -> Send Notification to all
-//        token = "fef9ae99842c1714c6749dc4106c3ceb61b9f08c388ef8a2ad261d1acdcf4526";
-//        String message = "Das Fablab ist hat gerade geöffnet";
-//        try {
-//            applePushService.sendpush(message, token);
-//        } catch (InterruptedException e) {
-//            e.printStackTrace();
-//        } catch (MalformedTokenStringException e) {
-//            e.printStackTrace();
-//        }
+        String message = "Das Fablab hat gerade geöffnet";
+        for(String token : tokens){
+            try {
+                applePushService.sendpush(message, token);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } catch (MalformedTokenStringException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     @Override
     public void sendCartStautsChanged(String token, CartStatus status) {
-
-    }
-
-
-    //Use push Notifications
-    public void nofityfiMeOnNextTimeDoorOpens(String token){
-
+        if(token.length() > 0) {
+            String message = "Warenkorb Status: " + status.toString();
+            System.out.println("APPLE PUSH FOR CARTSTATUS: " + token + " Message: " + message);
+            try {
+                applePushService.sendpush(message, token);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } catch (MalformedTokenStringException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
 

6c41d1993a268fc40115654e2815064a6d951318
Emanuel Eimer <mail@eeimer.de>
2015-08-27 20:28:54 +0200

    added endpoint to test Push (spaceAPI is not triggered @ devServer)

--- src/main/java/de/fau/cs/mad/fablab/rest/server/resources/PushResource.java
+++ src/main/java/de/fau/cs/mad/fablab/rest/server/resources/PushResource.java
@@ -3,21 +3,31 @@ package de.fau.cs.mad.fablab.rest.server.resources;
 import de.fau.cs.mad.fablab.rest.api.PushApi;
 import de.fau.cs.mad.fablab.rest.core.PushToken;
 import de.fau.cs.mad.fablab.rest.server.core.pushservice.PushFacade;
+import io.dropwizard.hibernate.UnitOfWork;
 
 import javax.ws.rs.core.Response;
 
 
 public class PushResource implements PushApi{
 
+    @UnitOfWork
     @Override
     public Response subscribeDoorOpensNextTime(PushToken pushToken) {
         PushFacade.getInstance().subscribeDoorOpensNextTime(pushToken);
         return Response.ok().build();
     }
 
+    @UnitOfWork
     @Override
     public Response unsubscribeDoorOpensNextTime(PushToken pushToken) {
         PushFacade.getInstance().unsubscribeDoorOpensNextTime(pushToken);
+        return Response.ok().build();
+    }
+
+    @UnitOfWork
+    @Override
+    public Response test() {
+        PushFacade.getInstance().fablabDoorJustOpened();
         return Response.ok().build();
     }
 }

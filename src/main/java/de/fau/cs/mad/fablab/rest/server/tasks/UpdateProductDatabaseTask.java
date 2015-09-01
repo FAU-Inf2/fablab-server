e47deea7825f32f7c1e904c31a5f4e15ffd3299d
Sebastian Haubner <sebastian.haubner@fau.de>
2015-09-01 15:40:24 +0200

    get hibernate session in UpdateProductDatabaseTask

--- src/main/java/de/fau/cs/mad/fablab/rest/server/tasks/UpdateProductDatabaseTask.java
+++ src/main/java/de/fau/cs/mad/fablab/rest/server/tasks/UpdateProductDatabaseTask.java
@@ -6,9 +6,11 @@ import de.fau.cs.mad.fablab.rest.server.core.ProductDAO;
 import de.fau.cs.mad.fablab.rest.server.core.openerp.OpenErpClient;
 import de.fau.cs.mad.fablab.rest.server.core.openerp.OpenErpException;
 import de.fau.cs.mad.fablab.rest.server.core.openerp.OpenErpInterface;
-import io.dropwizard.hibernate.UnitOfWork;
 import io.dropwizard.servlets.tasks.Task;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.context.internal.ManagedSessionContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -19,23 +21,40 @@ public class UpdateProductDatabaseTask extends Task {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(UpdateProductDatabaseTask.class);
     private ProductDAO dao;
+    private SessionFactory mFactory;
 
     public UpdateProductDatabaseTask(SessionFactory factory) {
         super("UpdateProductDatabaseTask");
+        mFactory = factory;
         dao = new ProductDAO(factory);
     }
 
-    @UnitOfWork
     @Override
     public void execute(ImmutableMultimap<String, String> parameters, PrintWriter output) throws Exception {
-        initializeDao();
+        final Session session = mFactory.openSession();
+        try {
+            ManagedSessionContext.bind(session);
+            Transaction transaction = session.beginTransaction();
+            try {
+                LOGGER.info("Initializing Product DAO");
+                initializeDao();
+                transaction.commit();
+                LOGGER.info("Initializing Product DAO SUCCESSFUL");
+            } catch (Exception e) {
+                transaction.rollback();
+                LOGGER.error("Initializing Product DAO ERROR!");
+                throw new RuntimeException("Unable to update Product Database \n\n"+e.getMessage());
+            }
+        } finally {
+            session.close();
+            ManagedSessionContext.unbind(mFactory);
+        }
     }
 
     /***
      * Use this method to create or update the dao store
      */
     private void initializeDao(){
-        LOGGER.info("Initializing Product DAO");
         OpenErpInterface openErp = OpenErpClient.getInstance();
         List<Product> products;
         try {
@@ -45,8 +64,6 @@ public class UpdateProductDatabaseTask extends Task {
             }
         } catch (OpenErpException e) {
             e.printStackTrace();
-            LOGGER.error("Initializing Product DAO ERROR!");
         }
-        LOGGER.info("Initializing Product DAO SUCCESSFUL");
     }
 }

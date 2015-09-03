cbb10ccbfdd8b048464b31470383c64ca9795a44
Max Jalowski <max.jalowski@fau.de>
2015-09-03 23:00:50 +0200

    Updated drupal-client: docker-drupal isn't needed anymore. Tools list gets extracted from the normal fablab-homepage

--- src/main/java/de/fau/cs/mad/fablab/rest/server/core/drupal/DrupalClient.java
+++ src/main/java/de/fau/cs/mad/fablab/rest/server/core/drupal/DrupalClient.java
@@ -6,11 +6,14 @@ import de.fau.cs.mad.fablab.rest.server.configuration.GeneralDataConfiguration;
 import de.fau.cs.mad.fablab.rest.server.configuration.NewsConfiguration;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.io.IOException;
 import java.util.Date;
 import java.util.LinkedHashMap;
 import java.util.LinkedList;
@@ -54,10 +57,9 @@ public class DrupalClient implements DrupalInterface {
                     "using the class NewsConfiguration.\n");
             System.exit(1);
         }
-
-        baseUrl = config.getUrl() + ":" + config.getPort() + "/" + config.getNodeEndpoint();
-        toolsUrl = baseUrl + JSON + "/?parameters[type]=tool&page=";
+        
         fabUrl = dataConfig.getFabUrl();
+        toolsUrl = fabUrl + "/tool";
     }
 
     public static void setConfiguration(NewsConfiguration c, GeneralDataConfiguration dc) {
@@ -107,74 +109,50 @@ public class DrupalClient implements DrupalInterface {
      * Updates the tool-List
      */
     private void updateToolList() {
-        LinkedList<FabTool> allTools;
-        LinkedHashMap<Integer, FabTool> tools = new LinkedHashMap<>();
+        LinkedList<FabTool> allTools = new LinkedList<>();
 
-        Client client = ClientBuilder.newClient().register(JacksonJsonProvider.class);
+        Document doc = null;
+        try {
+            doc = Jsoup.connect(toolsUrl).get();
+            doc.setBaseUri(fabUrl);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        Elements toolList = doc.select("ul[class=view-tools-list]").select("li");
 
-        String url = "";
-        Response jsonResponse = null;
-        DrupalNodeShort[] nodes = null;
-        int page = 0;
-
-        do {
-            url = toolsUrl + page;
-            jsonResponse = client.target(url).request(MediaType.APPLICATION_JSON).get();
-
-            if (jsonResponse.getStatus() != 200) {
-                throw new RuntimeException("Failed: HTTP Error: " + jsonResponse.getStatus() + ". URL was " + url);
+        int counter = 0;
+        for (Element li: toolList) {
+            FabTool tool = getToolFromLiElement(li);
+            if (tool != null) {
+                tool.setId(counter++);
+                allTools.add(tool);
             }
-
-            nodes = jsonResponse.readEntity(DrupalNodeShort[].class);
-
-            for (DrupalNodeShort node : nodes) {
-                int nid = node.getNid();
-                if (!tools.containsKey(nid)) tools.put(nid, getToolById(nid));
-            }
-
-            page++;
-        } while (nodes.length > 0);
-
-        allTools = new LinkedList<>(tools.values());
+        }
 
         if (allTools != null) {
             this.allTools = allTools;
             this.lastUpdate = new Date();
         }
-
     }
 
-    /***
-     * Gets the tool-node from the Drupal with 'id' and parses it into a {@link FabTool}-Object
-     *
-     * @param id the tool id
-     * @return a {@link FabTool}-Object
-     */
-    private FabTool getToolById(long id) {
-        String url = baseUrl + "/" + id + JSON;
-        Client client = ClientBuilder.newClient().register(JacksonJsonProvider.class);
+    private FabTool getToolFromLiElement(Element li) {
+        Element a = li.select("h2[class=title]").select("a").first();
+        Element content = li.select("div[class=\"content clearfix\"]").first();
 
-        Response jsonResponse = client.target(url).request(MediaType.APPLICATION_JSON).get();
-        if (jsonResponse.getStatus() != 200) {
-            throw new RuntimeException("Failed: HTTP Error: " + jsonResponse.getStatus() + ". URL was " + url);
-        }
-
-        DrupalNode node = jsonResponse.readEntity(DrupalNode.class);
-        return getToolFromNode(node);
-    }
-
-    private FabTool getToolFromNode(DrupalNode node) {
-        String body = node.getBody();
-        Document doc = Jsoup.parse(body, fabUrl);
+        if (a == null || content == null) return null;
 
         FabTool tool = new FabTool();
-        tool.setId(node.getNid());
-        tool.setTitle(node.getTitle());
-        tool.setDescription(node.getTeaser());
-        tool.setDetails(HTMLHelper.parseBody(doc, false));
-        tool.setLink(fabUrl + "/" + node.getPath());
-        tool.setLinkToPicture(null);
-
+        tool.setTitle(a.text());
+        tool.setDescription(content.text());
+        tool.setDetails(content.toString());
+        tool.setLink(a.attr("abs:href"));
+        tool.setLinkToPicture(getImageLink(content));
         return tool;
+    }
+
+    private String getImageLink(Element content) {
+        Element image = content.select("img").first();
+        if (image != null) return image.attr("abs:src");
+        else return null;
     }
 }

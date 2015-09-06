c8df7e89d79b1851d4412879c2448e9049782e45
Philip Kanhäußer <git@mail.philip.xers.de>
2015-09-06 17:21:54 +0200

    Added user/token authentication to ToolUsageResource.

--- src/main/java/de/fau/cs/mad/fablab/rest/server/resources/ToolUsageResource.java
+++ src/main/java/de/fau/cs/mad/fablab/rest/server/resources/ToolUsageResource.java
@@ -1,8 +1,11 @@
 package de.fau.cs.mad.fablab.rest.server.resources;
 
 import de.fau.cs.mad.fablab.rest.api.ToolUsageApi;
+import de.fau.cs.mad.fablab.rest.core.Roles;
 import de.fau.cs.mad.fablab.rest.core.ToolUsage;
+import de.fau.cs.mad.fablab.rest.core.User;
 import de.fau.cs.mad.fablab.rest.server.core.toolusage.ToolUsageFacade;
+import de.fau.cs.mad.fablab.rest.server.exceptions.Http401Exception;
 import de.fau.cs.mad.fablab.rest.server.exceptions.Http404Exception;
 import io.dropwizard.hibernate.UnitOfWork;
 
@@ -21,8 +24,22 @@ public class ToolUsageResource implements ToolUsageApi {
 
     @Override
     @UnitOfWork
-    public ToolUsage addUsage(long toolId, ToolUsage usage) {
+    public ToolUsage addUsage(User user, String token, long toolId, ToolUsage usage) {
         usage.setToolId(toolId);
+
+        if (user == null && (token == null || token.isEmpty()))
+            throw new Http401Exception("Authentication or token required.");
+
+        else if (user != null) {
+            usage.setUser(user.getUsername());
+        }
+        else if (!token.isEmpty()) {
+            usage.setToken(token);
+        }
+        else {
+            throw new Http401Exception("Authentication or token required.");
+        }
+
         return mFacade.create(usage);
     }
 
@@ -45,7 +62,26 @@ public class ToolUsageResource implements ToolUsageApi {
 
     @Override
     @UnitOfWork
-    public Response removeUsage(long toolId, long usageId) {
+    public Response removeUsage(User user, String token, long toolId, long usageId) {
+
+        if (user == null && (token == null || token.isEmpty()))
+            throw new Http401Exception("Authentication or token required.");
+
+        ToolUsage usage = mFacade.getUsage(toolId, usageId);
+        if (usage == null)
+            throw new Http404Exception("Usage not found");
+
+        if (user != null) {
+            if (!usage.getUser().equals(user.getUsername()) && !user.hasRole(Roles.ADMIN))
+                throw new Http401Exception("Username not identical.");
+        }
+        else if (!token.isEmpty()) {
+            if (!usage.getToken().equals(token))
+                throw new Http401Exception("Token not identical.");
+        }
+        else {
+            throw new Http401Exception("Authentication or token required.");
+        }
 
         if (mFacade.removeUsage(toolId, usageId)) {
             return Response.ok().build();
@@ -56,7 +92,11 @@ public class ToolUsageResource implements ToolUsageApi {
 
     @Override
     @UnitOfWork
-    public Response removeUsagesForTool(long toolId) {
+    public Response removeUsagesForTool(User user, long toolId) {
+        if (!user.hasRole(Roles.ADMIN)){
+            throw new Http401Exception("Only for admins.");
+        }
+
         mFacade.clearUsageForTool(toolId);
 
         return Response.ok().build();
@@ -67,7 +107,11 @@ public class ToolUsageResource implements ToolUsageApi {
     /**
      * @see de.fau.cs.mad.fablab.rest.server.core.toolusage.ToolUsageDAO#moveAfter(ToolUsage, ToolUsage)
      */
-    public Response moveAfter(long toolId, long usageId, long afterId) {
+    public Response moveAfter(User user, long toolId, long usageId, long afterId) {
+
+        if (!user.hasRole(Roles.ADMIN)) {
+            throw new Http401Exception("Only for admins.");
+        }
 
         if (mFacade.moveAfter(toolId, usageId, afterId))
             return Response.ok().build();

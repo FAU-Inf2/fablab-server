bb241c298c2f862bab71469a65685553bf568ef0
Max Jalowski <max.jalowski@fau.de>
2015-09-15 20:08:03 +0200

    image upload from docker should work now.

--- scripts/imgToGist.sh
+++ scripts/imgToGist.sh
@@ -5,6 +5,10 @@ repo_id=$2
 inputfile=$3
 filename=$4
 
+#configure git for image upload
+git config --global user.email "fablab@i2.cs.fau.de"
+git config --global user.name "fablab-server"
+
 git clone https://$token@gist.github.com/$repo_id.git
 mv $inputfile $repo_id/$filename
 cd $repo_id

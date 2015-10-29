03e70bf05b7b21e2de366b56c993f2e8c8b73e59
julian <julian@jneureuther.de>
2015-10-30 00:10:52 +0100

    remove unused docker containers

--- docker/database/create.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-echo "Creating database container..."
-docker build -t container-fablab-database .
--- docker/drupal7/cleanup.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-docker stop mysql-for-drupal7
-docker rm mysql-for-drupal7 fablab-drupal7-v1 
-#docker rmi mysql-for-drupal7 fablab-drupal7
